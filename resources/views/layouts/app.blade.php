ed53b640b8ea9f71ec7337e2038a47dde0fede1d
JouryABDULLAH <jours-222@outlook.com>
2023-07-30 14:40:57 -0700

    directory stucture update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>CV</title>
+</head>
+<body>
+    
+    <header>
+        <div>
+            <h1>Joori Abdullah</h1>
+            <p>CS enthuasist and web developer </p>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </header> 
+
+    <main>
+        <nav>
+            <ul>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Skills</li>
+            </ul>
+        </nav>
+        <div class="content">
+
+        </div>
+    </main>
+
+</body>
+</html>
