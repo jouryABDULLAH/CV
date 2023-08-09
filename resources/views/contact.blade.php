47d30b0c44376cdbbf832a176a5fc0300b4c6735
JouryABDULLAH <jours-222@outlook.com>
2023-08-08 17:24:46 -0700

    fix resposivness && create contact page layout with styles

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
     <title>Contact me</title>
 </head>
 <body>
@@ -12,7 +13,31 @@
     @extends('layouts.app')
 
     @section('content')
-        <p>This is contact.</p>
+
+        <h2>Get In Touch</h2>
+        <hr>
+        <br>
+        <form action="">
+            <div class="input-container">
+                <label class="form-label" for="name">Enter Your Name</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="input-container">
+                <label class="form-label" for="email">Enter Your E-Mail</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div class="input-container">
+                <label class="form-label" for="subject">Subject</label>
+                <input type="text" name="subject" id="subject">
+            </div>
+            <div class="input-container">
+                <label class="form-label" for="message">Message</label>
+                <textarea name="message" id="message" ></textarea>
+            </div>
+            <div class="button-container">
+                <button type="submit">Send</button>
+            </div>
+        </form>
     @endsection
     
 </body>
