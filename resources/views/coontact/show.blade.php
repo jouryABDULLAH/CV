baf3914440726aa431293ec153a01220b17b84db
JouryABDULLAH <jours-222@outlook.com>
2023-08-10 07:25:08 -0700

    add styles to /coontacts/show page

--- resources/views/coontact/show.blade.php
+++ resources/views/coontact/show.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/showContacts.css') }}">
     <title>show contact</title>
 </head>
 <body>
@@ -13,21 +14,26 @@
 
     @section('content')
         <ul>
-            <a href="{{ route('coontact.edit', $contact->id)}}">
                 <li>
                     <p>
-                        {{$contact['contact_name']}} sent a message with the following subject: <strong>{{$contact['contact_subject']}}</strong>
+                        <strong>{{$contact['contact_name']}}</strong> sent a message with the following subject: <strong>{{$contact['contact_subject']}}</strong>
                     </p>
                    
-                    <form action="{{route('coontact.destroy',$contact->id)}}" method="post">
-                        @csrf 
-                        @method('DELETE')
-                        <button type="submit" class="delete-btn">
-                            X
+                    <div class="buttons">
+                        <form action="{{route('coontact.destroy',$contact->id)}}" method="post">
+                            @csrf 
+                            @method('DELETE')
+                            <button type="submit" class="btn">
+                                Delete
+                            </button>
+                        </form>
+                        <button class="btn edit">
+                            <a href="{{ route('coontact.edit', $contact->id)}}">
+                                Edit
+                            </a>
                         </button>
-                    </form>
+                    </div>
                 </li>
-            </a>
         </ul>
     @endsection
     
